4d6e9260d7a2c7ae17fd242ed190ae34765668ac
Yaici Walid <hw_yaici@esi.dz>
2019-11-23 12:47:25 +0100

    ajout des balises Route et modification du css de App

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,26 +1,57 @@
+// App.js
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SideBar from './Sidebar';
+import Square from './Listeabs.js';
+import './css/App.css';
+import Footer from './footer.js';
+import Navb from './Navbar.js';
+import Login from './Login.js';
+import Formabs from './Formabs.js';
 
-function App() {
+
+var email;
+var lien;
+var group;
+var modul=["ALG","ANA1","ANA2","ANA3"];
+var eleves=[{mat:170186,nom:'yaici',prenom:'walid',absent:null},
+{mat:170186,nom:'yaici',prenom:'walid',absent:null},
+{mat:170190,nom:'deroues',prenom:'noufel',absent:null},
+{mat:170001,nom:'etic',prenom:'dream team',absent:null},
+{mat:170125,nom:'guendouz',prenom:'yanis',absent:null},]
+
+export default class App extends React.Component {
+  
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    <div id="App">
+      <p>{email}</p>
+      <Route path="/" exact render={()=>(<Login onClick={this.handleLoginClick} link={lien}/>)}/>
+      <Route path="/home" render={() => (<Navb id={email} />)} />
+      <Route path="/home" component={SideBar} />
+      <div id="page-wrap">
+      <Route path="/home/Form" render={() => (<Formabs link="/home/Abs" modules={modul}/>)}/>
+        <Route path="/home/Abs" render={() => (<Square eleves={eleves} />)}/>
+      </div>
+      <Footer/>
+    </div>
+
+
+  )}
+
+  handleLoginClick(em,admin,ens,etu){
+    
+    if(!true){
+      lien="";
+      alert("Erreur d'authentification");
+      }else {
+      if(ens) lien="/home/Form";
+      email=em;
+      console.log(email);
+    }
+  }
+
+ 
+}
