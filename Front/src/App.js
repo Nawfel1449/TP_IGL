7cedc28b38a394eb5807537da5cd68e8f0e62c1c
yaici walid <hw_yaici@esi.dz>
2020-01-03 23:00:47 +0100

    ajout des requete api

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,5 +1,4 @@
 // App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SideBar from './Sidebar';
@@ -14,18 +13,21 @@ import ListeEtudiant from './ListeEtudiant.js'
 import axios from 'axios';
 import {Redirect} from 'react-router';
 
-var email;
 var lien;
-var group;
-var modul=["ALG","ANA1","ANA2","ANA3"];
+var group=[];
+var modul=[];
 var eleves=[];
-
-export default class App extends React.Component {
+var emai="";
+ /**@class la classe principale pour gerer les differents composants*/
+class App extends React.Component {
 
 	constructor(props) {
     super(props)
+		this.state={email:""}
     this.handleFormabsClick= this.handleFormabsClick.bind(this)
 		this.handleFormajoutClick= this.handleFormajoutClick.bind(this)
+		this.handleLoginClick= this.handleLoginClick.bind(this)
+
 
   }
 
@@ -33,54 +35,108 @@ export default class App extends React.Component {
   return (
 
     <div id="App">
-      <p>{email}</p>
       <Route path="/" exact render={()=>(<Login onClick={this.handleLoginClick} link={lien}/>)}/>
-      <Route path="/home" render={() => (<Navb id={email} />)} />
+      <Route path="/home" render={() => (<Navb id={this.state.email} />)} />
       <Route path="/home" component={SideBar} />
       <div id="page-wrap">
-        <Route path="/home/Form" render={() => (<Formabs link="/home/Abs" modules={modul} onClick={this.handleFormabsClick}/>)}/>
+        <Route path="/home/Form" render={() => (<Formabs  modules={modul} groupes={group} onClick={this.handleFormabsClick}/>)}/>
         <Route path="/home/Abs" render={() => (<Square eleves={eleves} />)}/>
-				<Route path="/home/AjouterEtudiant" render={() => (<FormAjout link="/home/Listetudiant" onClick={this.handleFormajoutClick}/>)}/>
+				<Route path="/home/AjouterEtudiant" render={() => (<FormAjout groupes={group} onClick={this.handleFormajoutClick}/>)}/>
 				<Route path="/home/Listetudiant" render={() => (<ListeEtudiant eleves={eleves} />)}/>
-
       </div>
       <Footer/>
     </div>
 
 
   )}
+	/**
+	* appelé si le login c'est fait avec succes et transmet l'email dans le state de l'app
+	* @param {string} em email du compte connecté.
+	* @param{boolean} authen booleen pour indiquer que l'authentification c'est faite correctement ou non
+	* @param{object[]} profile contient le profile de l'utilisateur connecté
+	* @param{boolean} adm booleen pour indiqer si le compte est administrateur
+	*/
+  handleLoginClick(em,authen,profile,adm){
+		if(authen){
+			emai=em;
+			if(adm){
+				axios.get(`http://127.0.0.1:7000/groupes_list/`)
+					.then(res => {
+							group=res.data;
+							this.setState({});
+		      })
+			}else{
+				axios.get(`http://127.0.0.1:8000`+profile)
+					.then(res => {
+						if(res.data.Group1!==""){
+							group[0]=res.data.Group1;
+							if(res.data.Group2!==""){
+								group[1]=res.data.Group2;
+								if(res.data.Group3!==""){
+									group[2]=res.data.Group3;
+									if(res.data.Group4!=="")group[3]=res.data.Group4;
+								}
+							}
+						}
+						if(res.data.Module1!==""){
+							modul[0]=res.data.Module1;
+							if(res.data.Module2!==""){
+								modul[1]=res.data.Module2;
+								if(res.data.Module3!=="") modul[2]=res.data.Module3;
+							}
 
-  handleLoginClick(em,admin,ens,etu){
-    if(!true){
-      alert("Erreur d'authentification");
-      }else {
-      if(ens) lien="/home/Form";
-			else if (admin) lien="/home/AjouterEtudiant";
-      email=em;
-			this.props.history.push('/dashboard')
-      console.log(email);
-    }
+						}
+						this.setState({});
+
+		      })
+			}
+
+		}
+
   }
+	/**
+	* lance   une requete pour afficher la liste des etudiants d'un meme groupe
+	* @param {string} grp le groupe de l'etudiant a ajouter.
 
-	handleFormabsClick(){
-		axios.get(`http://127.0.0.1:8000/api/Student`)
+	*/
+
+	handleFormabsClick(grp){
+		axios.get(`http://127.0.0.1:7000/group/`+grp)
 			.then(res => {
         eleves=res.data;
 				this.setState({});
 
       })
+			console.log(emai);
 	}
 
-	handleFormajoutClick(groupe){
-		group=groupe;
-		eleves=[];
-		axios.get(`http://127.0.0.1:8000/api/Student`)
+	/**
+             * lance une requete d'ajout d'un etudiant a un groupe puis une requete pour afficher la liste des etudiants du meme groupe
+             * @param {string} grp le groupe de l'etudiant a ajouter.
+             * @param {string}  mat matricule de l'etudiant.
+             * @param {string} name nom de l'etudiant.
+						 *@param {string} pre prenom de l'etudiant.
+             */
+	handleFormajoutClick(grp,mat,name,pre){
+		var email=pre[0]+'_'+name+"@esi.dz";
+		axios.post('http://127.0.0.1:7000/etudiant/', {
+    matricule: mat,
+    nom: name,
+    prenom: pre,
+    email: email,
+    groupe: grp
+  })
+  .then(response=>{
+		axios.get(`http://127.0.0.1:7000/group/`+grp)
 			.then(res => {
         eleves=res.data;
 				this.setState({});
 
       })
+  })
+
 	}
 
 
 }
+export default App;
--- Front/src/Login.js
+++ Front/src/Login.js
@@ -3,27 +3,88 @@ import './css/Login.css';
 //import { browserHistory } from 'react-router-dom';
 import {Button,Form,FormGroup,Label,Input} from 'reactstrap';
 import {FacebookLoginButton} from 'react-social-login-buttons';
-import { BrowserRouter as Router, Route, Link,Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
+var authen=false;
+var profile="";
+var is_adm=false;
 
-var admin;
-var ens;
-var etu;
-var authenntification=true;
-var email;
+/**@class composant pour gerer le login
+
+*/
 
 class Login extends Component {
 
+	constructor(props){
+		super(props);
+		this.state={admin:false,ens:false,etu:false,link:"/",email:false,mdp:false};
+		this.handleCheckClic=this.handleCheckClic.bind(this);
+		this.handlelogClick=this.handlelogClick.bind(this);
 
+
+	}
+
+	/**
+	* determine le type d'utilisateur voulant ce connecter
+	* @param {boolean} adm vrai si l'utilisateur veut ce connecter en tant qu'administrateur.
+	* @param {boolean} en vrai si l'utilisateur veut ce connecter en tant qu'enseignant.
+	* @param {boolean} et vrai si l'utilisateur veut ce connecter en tant qu'etudiant.
+	*/
 	handleCheckClic(adm,en,et){
-		admin=adm;
-		ens=en;
-		etu=et;
-		//if(admin) link="/home";
+		this.setState({admin:adm,ens:en,etu:et});
+		if(this.state.email&&this.state.mdp){
+			console.log(document.getElementById("pwd").value);
+			axios.get('http://127.0.0.1:8000/login/'+document.getElementById("mail").value+'/'+document.getElementById("pwd").value
+	    ).then(res => {
+	        console.log(res);
+					if (res.data.profile!==null){
+						if (this.state.admin&&res.data.is_admin) {
+							this.setState({link:"/home/AjouterEtudiant"});
+							is_adm=true;
+							authen=true;
+						}
+						else if (this.state.ens){
+							this.setState({link:"/home/Form"});
+							authen=true;
+						}
+						else if(this.state.etu){
+							authen=true;
+							this.setState({link:"/home"});
+						}
+						else authen=false;
+					}
+					else authen=false;
+
+	      }).catch(err=>{console.log(err);})
+
+		}
+	}
+	/**
+	* verifie que tout les champ sont remplit et fait une requete de connexion.
+	*/
+	handlelogClick(){
+		document.getElementById("mail").value==="" ? this.setState({email:false}):this.setState({email:true});
+		document.getElementById("pwd").value===""?this.setState({mdp:false}):this.setState({mdp:true});
+		if(this.state.email&&this.state.mdp&&(this.state.admin||this.state.ens||this.state.etu)){
+			if (this.state.admin) this.setState({link:"/home/AjouterEtudiant"});
+			else if (this.state.ens) this.setState({link:"/home/Form"});
+		}
+		/*if(this.state.email&&this.state.mdp&&(this.state.admin||this.state.ens||this.state.etu)){
+			axios.post(`http://127.0.0.1:8000/login`,{
+				username: document.getElementById("mail").value,
+				password:document.getElementById("pwd").value
+				}).then(res => {
+					console.log(res);
+					if (this.state.admin) this.setState({link:"/home/AjouterEtudiant"});
+					else if (this.state.ens) this.setState({link:"/home/Form"});
+				})
+		}*/
+
 	}
 
 
-	
+
 
 
 	render(){
@@ -33,30 +94,30 @@ class Login extends Component {
 				<h1 className="font-weight-bold text-center" id="esiH1">
 					ESI SCOLARITE
 				</h1>
-				
+
 				<h2 className="text-center" id="esiH2">Bienvenue</h2>
 				<img id="cadenas"src="./cadenas.png" />
 				<FormGroup>
 					<Label>Email</Label>
-					<Input type="email" placeholder="Email" id="mail"/>
+					<Input type="email" placeholder="Email" id="mail" onChange={this.handlelogClick}/>
 				</FormGroup>
 				<FormGroup>
 					<Label>Password</Label>
-					<Input type="Password" id="pwd" placeholder="Password"/>
+					<Input type="Password" id="pwd" placeholder="Password" onChange={this.handlelogClick}/>
 				</FormGroup>
 				<label class="container">Admin
-				  <input type="radio"  name="radio" onClick={()=>this.handleCheckClic(true,false,false)}/>
+				  <input type="radio"  name="radio" onChange={()=>this.handleCheckClic(true,false,false)}/>
 				  <span class="checkmark"></span>
 				</label>
 				<label class="container">Enseignant
-				  <input type="radio"  name="radio" onClick={()=>this.handleCheckClic(false,true,false)}/>
+				  <input type="radio"  name="radio" onChange={()=>this.handleCheckClic(false,true,false)}/>
 				  <span class="checkmark"></span>
 				</label>
 				<label class="container">etudiant
-				  <input type="radio"  name="radio" onClick={()=>this.handleCheckClic(false,false,true)}/>
+				  <input type="radio"  name="radio" onChange={()=>this.handleCheckClic(false,false,true)}/>
 				  <span class="checkmark"></span>
 				</label>
-				<Link to={this.props.link}><Button className="btn-lg btn-danger btn-block mt-4 btn-custum" onClick={()=>this.props.onClick(document.getElementById("mail").value,admin,ens,etu)}>
+				<Link to={this.state.link}><Button className="btn-lg btn-danger btn-block mt-4 btn-custum" onClick={()=>this.props.onClick(document.getElementById("mail").value,authen,profile,is_adm)}>
 					Log in </Button></Link>
 				<div className="text-center">
 					<a href="#" >Sign up</a>
